6a2cb8652707d428a9eb0bf646c08f342d68efdd
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-09 00:00:29 +0700

    Fixing Travis Error

--- edii_kbn/reports/account_debt_collection_handling.py
+++ edii_kbn/reports/account_debt_collection_handling.py
@@ -2,10 +2,10 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import edii_kbn_base_parser as Base
+from edii_kbn_base_parser import EDIKBNBaseParser as Base
 
 
-class Parser(Base.EDIKBNBaseParser):
+class Parser(Base):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.context = context
